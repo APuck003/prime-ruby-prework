e25edd84365f79bc2d5a751c93685046b2f29f51
Austin Puckett <puckett_austin@yahoo.com>
2018-10-04 01:29:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif n <= 3
+    return true
+  elsif (2..num/2).none? do |x|
+    num % x == 0
+    return false
+  else
+    return true
+  end
+
+end
+end
